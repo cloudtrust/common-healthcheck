2faa933cacf4e4aa8bfcc6e7e5f899b96e6df6d8
Johan Droz <johan.droz@gmail.com>
2018-07-24 20:31:22 +0200

    Add better error name for validation middleware

--- validation.go
+++ validation.go
@@ -3,6 +3,7 @@ package common
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 )
 
 // MakeValidationMiddleware makes a middleware that validate the health check name comming from
@@ -21,19 +22,21 @@ type validationMW struct {
 	next        HealthChecker
 }
 
+// ErrInvalidHCName is the error returned when there is a health request for
+// an unknown healthcheck name.
 type ErrInvalidHCName struct {
 	s string
 }
 
 func (e *ErrInvalidHCName) Error() string {
-	return e.s
+	return fmt.Sprintf("no health check with name '%s'", e.s)
 }
 
 func (m *validationMW) HealthCheck(ctx context.Context, name string) (json.RawMessage, error) {
 	// Check health check name validity.
 	var _, ok = m.validValues[name]
 	if !ok {
-		return nil, &ErrInvalidHCName{}
+		return nil, &ErrInvalidHCName{name}
 	}
 
 	return m.next.HealthCheck(ctx, name)
